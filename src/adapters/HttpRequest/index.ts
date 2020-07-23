5f118de92cf03e6546a527bcef580457b510de5e
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-23 16:05:40 -0300

    docs: updating readme

--- src/adapters/HttpRequest/index.ts
+++ src/adapters/HttpRequest/index.ts
@@ -1,11 +1,7 @@
 import AxiosObservableAdapter from "./AxiosObservable.adapter";
 import IHttpRequest from "./IHttpRequest.adapter";
 
-const baseUrl = process.env.REACT_APP_API_URL;
-
-if (!baseUrl) {
-  throw new Error("Missing config REACT_APP_API_URL");
-}
+const baseUrl = process.env.API_URL || "https://jsonplaceholder.typicode.com";
 
 const httpRequest: IHttpRequest = new AxiosObservableAdapter(baseUrl);
 
