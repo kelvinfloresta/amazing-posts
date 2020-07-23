7cd1c9f728fab795f0be0992263deacaab024756
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-23 16:18:05 -0300

    tests: ensure Post click on ListPosts

--- src/components/ListPost/ListPost.test.tsx
+++ src/components/ListPost/ListPost.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import ListPost from "./ListPost";
-import { render, fireEvent } from "@testing-library/react";
 
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
@@ -9,43 +8,38 @@ import { ViewPost } from "components/ViewPost/ViewPost";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const mockHistoryPush = jest.fn();
-
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useHistory: () => ({
-    push: mockHistoryPush,
-  }),
-}));
-
-beforeEach(() => {
-  mockHistoryPush.mockReset();
-});
-
 describe("Name of the group", () => {
-  it("should navigate correctly to the route when clicked", () => {
+  it("should call onClickPost when click on Post", () => {
     const post = { id: 1, body: "Body", title: "Title" };
-    const wrapper = Enzyme.shallow(
+    const onClickPostSpy = jest.fn();
+    const wrapper = Enzyme.mount(
       <ListPost
         isLoading={false}
         posts={[post]}
         retry={console.log}
         error={null}
+        onClickPost={onClickPostSpy}
       />
     );
     const component = wrapper.find(ViewPost);
     component.simulate("click");
-    const expectedRoute = "/" + post.id;
-    expect(mockHistoryPush).toHaveBeenCalledWith(expectedRoute);
+    expect(onClickPostSpy).toHaveBeenCalledWith(post.id);
   });
 
-  it("should not navigate when loading", () => {
+  it("should not call onClickPost when Post is loading", () => {
     const post = { id: 1, body: "Body", title: "Title" };
-    const wrapper = Enzyme.shallow(
-      <ListPost isLoading posts={[]} retry={console.log} error={null} />
+    const onClickPostSpy = jest.fn();
+    const wrapper = Enzyme.mount(
+      <ListPost
+        isLoading
+        posts={[]}
+        retry={console.log}
+        error={null}
+        onClickPost={onClickPostSpy}
+      />
     );
     const component = wrapper.find(ViewPost).first();
     component.simulate("click");
-    expect(mockHistoryPush).not.toHaveBeenCalled();
+    expect(onClickPostSpy).not.toHaveBeenCalled();
   });
 });
