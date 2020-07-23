acc3d53c963b3a4b2b09067456ada069b6839ed0
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-23 15:28:07 -0300

    fix: responsive layout for ListPost component

--- src/components/ListPost/style.tsx
+++ src/components/ListPost/style.tsx
@@ -13,4 +13,12 @@ export const StyledListPost = withChildAsFunction(styled.div`
   grid-column-gap: 2rem;
   grid-row-gap: 4rem;
   grid-template-columns: repeat(3, max-content);
+
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(2, max-content);
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `);
