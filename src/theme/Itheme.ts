e8230b8aa44b02b8769361cace581d375a3f2e5e
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-23 15:06:26 -0300

    feat: add theme

---
+++ src/theme/Itheme.ts
@@ -0,0 +1,8 @@
+export interface ITheme {
+  primaryColor: string;
+  secondaryColor: string;
+  dangerColor: string;
+  textColor1: string;
+  textColor2: string;
+  textColor3: string;
+}
---
+++ src/theme/theme.ts
@@ -0,0 +1,12 @@
+import { ITheme } from "./Itheme";
+
+const theme: ITheme = {
+  primaryColor: "#00A0E4",
+  secondaryColor: "#FFD400",
+  dangerColor: "#dc3545",
+  textColor1: "#262626",
+  textColor2: "#555",
+  textColor3: "#6E6E6E",
+};
+
+export default theme;
