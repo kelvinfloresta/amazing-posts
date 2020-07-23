b06d3333aaa6ea5d95d64157984fa5446f84fbe6
Kelvin Floresta <kelvinf.contato@gmail.com>
2020-07-23 15:53:26 -0300

    fix: PostDetail responsive style

--- src/pages/PostDetail/style.ts
+++ src/pages/PostDetail/style.ts
@@ -1,11 +1,22 @@
 import styled from "styled-components";
 import { withChildAsFunction } from "components/withChildAsFunction";
+import { phone } from "device";
 
 export const PostActions = styled.div`
   display: flex;
   align-items: center;
   button {
     margin-left: 2rem;
+  }
+
+  @media (max-width: ${phone}) {
+    flex-direction: column;
+    height: 100px;
+    justify-content: space-between;
+
+    button {
+      margin-left: 0;
+    }
   }
 `;
 
@@ -14,6 +25,10 @@ export const FailedDeletePost = styled.span<{ hasError: boolean }>`
   color: ${(props) => props.theme.dangerColor};
   transition: opacity 0.3s;
   opacity: ${(props) => (props.hasError ? 1 : 0)};
+
+  @media (max-width: ${phone}) {
+    margin-left: 0;
+  }
 `;
 
 export const PostDetailContainer = withChildAsFunction(styled.div`
